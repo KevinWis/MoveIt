5b8f15947970cc71a2fce82d8d4525b7cf31bdae
kevin wischneski <kevinwischneski@gmail.com>
2021-02-24 17:24:16 -0300

    fix: improving styling and styled components

--- src/components/experienceBar/index.tsx
+++ src/components/experienceBar/index.tsx
@@ -1,14 +1,12 @@
-import { Header } from "./style";
+import { Header, CurrentXp, CurrentXpBar } from "./style";
 
 const ExperienceBar = () => {
   return (
     <Header>
       <span>0 xp</span>
       <div>
-        <div style={{ width: "50%" }} />
-        <span className="current-xp" style={{ left: "50%" }}>
-          300xp
-        </span>
+        <CurrentXpBar charge={50} />
+        <CurrentXp offsetX={50}>300xp</CurrentXp>
       </div>
       <span>600 xp</span>
     </Header>
