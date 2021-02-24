10d547cbd10463c427bd1d8ef4de895eb5118937
kevin wischneski <kevinwischneski@gmail.com>
2021-02-24 12:25:16 -0300

    feat(styling):adding main styling for the experience bar

---
+++ src/components/experienceBar/index.tsx
@@ -0,0 +1,17 @@
+import "./style.css";
+const ExperienceBar = () => {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }} />
+        <span className="current-xp" style={{ left: "50%" }}>
+          300xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+};
+
+export default ExperienceBar;
