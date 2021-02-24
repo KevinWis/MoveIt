ec2ad2f1a6b2e26082ecfe2b44197f1f59873cbf
kevin wischneski <kevinwischneski@gmail.com>
2021-02-24 12:13:55 -0300

    building the scaffold

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+    </>
+  );
+};
+
+export default Home;
