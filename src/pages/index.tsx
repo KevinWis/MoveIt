8e8bf552a2634d3dfda628a9c543146e103ffc6f
kevin wischneski <kevinwischneski@gmail.com>
2021-02-24 21:17:33 -0300

    fix:adding nextjs configuration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,7 @@ import Head from "next/head";
 const Home = () => {
   return (
     <>
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
+
     </>
   );
 };
