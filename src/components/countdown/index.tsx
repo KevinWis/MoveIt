dce7835a0a713055d7507c0e0ec80328271f0115
kevin wischneski <kevinwischneski@gmail.com>
2021-02-25 17:03:14 -0300

    feat(countdown): Adding logic and counter to the component. also fixing minor css mistakes

--- src/components/countdown/index.tsx
+++ src/components/countdown/index.tsx
@@ -1,17 +1,46 @@
-import { Container } from "./style";
+import { useEffect, useState } from "react";
+import { Container, Button } from "./style";
 const Countdown = () => {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (time % 60).toString().padEnd(2, "0");
+
+  const toggleCountdown = () => {
+    setActive(!active);
+  };
+  const count = () => {
+    if (time > 0 && active) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  };
+
+  useEffect(() => {
+    count();
+  }, [time, active]);
+
   return (
-    <Container>
-      <div className="timer">
-        <span>2</span>
-        <span>5</span>
-      </div>
-      <span>:</span>
-      <div className="timer">
-        <span>0</span>
-        <span>0</span>
-      </div>
-    </Container>
+    <div>
+      <Container>
+        <div className="timer">
+          <span>{minutes[0]}</span>
+          <span>{minutes[1]}</span>
+        </div>
+        <span>:</span>
+        <div className="timer">
+          <span>{seconds[0]}</span>
+          <span>{seconds[1]}</span>
+        </div>
+      </Container>
+      <Button onClick={toggleCountdown} type="button">
+        Iniciar
+      </Button>
+    </div>
   );
 };
 export default Countdown;
--- src/components/countdown/style.ts
+++ src/components/countdown/style.ts
@@ -13,6 +13,7 @@ export const Container = styled.div`
   }
 
   .timer {
+    flex: 1;
     display: flex;
     align-items: center;
 
@@ -26,7 +27,6 @@ export const Container = styled.div`
 
     span {
       flex: 1;
-
       &:first-child {
         border-right: 1px solid #f0f1f3;
       }
@@ -34,5 +34,31 @@ export const Container = styled.div`
         border-left: 1px solid #f0f1f3;
       }
     }
+  }
+`;
+export const Button = styled.button`
+  width: 100%;
+  height: 5rem;
+
+  margin-top: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: none;
+  border-radius: 5px;
+
+  background-color: var(--blue);
+  color: var(--white);
+
+  font-size: 1.25rem;
+  font-weight: 600;
+
+  transition: 0.35s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+
+  outline: none;
+  &:hover {
+    background-color: var(--blue-dark);
   }
 `;
