1a2407f6c451a00ab007cbf970b10fd4de794397
kevin wischneski <kevinwischneski@gmail.com>
2021-02-27 18:44:12 -0300

    fix(context): Fixing context issues

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,20 +4,25 @@ import Profile from "../components/profile";
 import CompletedChallenges from "../components/completedChallenges";
 import Countdown from "../components/countdown";
 import ChallengeBox from "../components/challengeBox";
+import { CountdownProvider } from "../contexts/countdownContext";
+import { Head } from "next/document";
+
 const Home = () => {
   return (
     <Container>
       <ExperienceBar />
-      <InnerContainer>
-        <div>
-          <Profile />
-          <CompletedChallenges />
-          <Countdown />
-        </div>
-        <div>
-          <ChallengeBox />
-        </div>
-      </InnerContainer>
+      <CountdownProvider>
+        <InnerContainer>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Countdown />
+          </div>
+          <div>
+            <ChallengeBox />
+          </div>
+        </InnerContainer>
+      </CountdownProvider>
     </Container>
   );
 };
