66618b661507bc21a44c3a7933b5c83fe8f6d409
kevin wischneski <kevinwischneski@gmail.com>
2021-02-27 17:14:58 -0300

    fix(completed-challenges): replacing fixed values for dynamic values

--- src/components/completedChallenges/index.tsx
+++ src/components/completedChallenges/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../../contexts/challengesContext";
 import { StyledCompletedChallenges } from "./style";
 
 const CompletedChallenges = () => {
+  const { challengesCompleted } = useContext(ChallengesContext);
   return (
     <StyledCompletedChallenges>
       <span>Desafios completos</span>
-      <span>05</span>
+      <span>{challengesCompleted}</span>
     </StyledCompletedChallenges>
   );
 };
